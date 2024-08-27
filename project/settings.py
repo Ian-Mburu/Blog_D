74b016f2a5deb6ccd840198788642c4d731a2604
Ian-Mburu <ianongaruiya@gmail.com>
2024-08-27 23:09:06 +0300

    settings

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
